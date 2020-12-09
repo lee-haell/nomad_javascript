feda3f431c91602b08066e3aed615490ec8db4d7
lee-haell <haeeell@naver.com>
2020-12-09 20:09:21 +0900

    To do list 만들기_상수

---
+++ todo.js
@@ -0,0 +1,5 @@
+const toDoFrom = document.querySelector(".js-toDoFrom"),
+    toDoInput = toDoFrom.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const SAVE_TODO = "toDo";
